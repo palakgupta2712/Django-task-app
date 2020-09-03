2777960e9b8d697a9fb894ca9eb03bd562cd3d54
Palak <palakgupta2712@gmail.com>
2020-09-04 02:31:52 +0530

    TaskList view created

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView, DetailView
+from .models import Task
 # Create your views here.
+
+class TaskList(ListView):
+    queryset = Task.objects.all().order_by('-created_on')
+    template_name = 'TaskList.html'
+
