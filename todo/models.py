9ac892ebc4d8428e714af5629eb62c4034262a7a
Palak <palakgupta2712@gmail.com>
2020-09-05 18:57:30 +0530

    Task and TaskList models changed

--- todo/models.py
+++ todo/models.py
@@ -1,29 +1,31 @@
 from django.db import models
+from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your models here.
-class TaskGroup(models.Model):
-    name = models.CharField(max_length=32)
-    slug = models.SlugField()
-
+class TaskList(models.Model):
+    name = models.CharField(max_length=60)
+    slug = models.SlugField(default="")
+   
     def __str__(self):
         return self.name
 
-
-STATUS =(
-    ("Todo", "Todo"),
-    ("Done", "Done")
-)
-class Task(models.Model):
-    title = models.CharField(max_length=64)
-    slug = models.SlugField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey(User, on_delete= models.CASCADE, related_name="created_by")
-    status = models.TextField(choices=STATUS, default = "Todo")
-    task_group = models.ForeignKey(TaskGroup, on_delete= models.CASCADE, related_name="task_group")
-
     class Meta:
-        ordering = ["-created_on"]
+        ordering = ["name"]
+        verbose_name_plural = "Task Lists"
+
+class Task(models.Model):
+    title = models.CharField(max_length=128)
+    task_list = models.ForeignKey(TaskList, on_delete=models.CASCADE, null=True)
+    created_date = models.DateField(default=timezone.now, blank=True, null=True)
+    completed = models.BooleanField(default=False)
+    completed_date = models.DateField(blank=True, null=True)
+    created_by = models.ForeignKey(
+        User,
+        blank=True,
+        related_name="todo_created_by",
+        on_delete=models.CASCADE,
+    )
 
     def __str__(self):
         return self.title
