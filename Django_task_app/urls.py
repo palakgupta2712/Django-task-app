a4281fc4d5aa3eda2a38e85b142e59c9b9bd3e57
Palak <palakgupta2712@gmail.com>
2020-09-06 00:06:47 +0530

    signup added

--- Django_task_app/urls.py
+++ Django_task_app/urls.py
@@ -20,4 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('todo.urls')),
     path('accounts/', include('django.contrib.auth.urls')), # login_update
+    path('accounts/', include('accounts.urls')), # signup_update
+
 ]
