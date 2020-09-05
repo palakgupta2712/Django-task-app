42a82b2d3f3ef5408f0b95ddc0af2ebb863930a7
Palak <palakgupta2712@gmail.com>
2020-09-06 00:00:14 +0530

    login and logout added

--- Django_task_app/settings.py
+++ Django_task_app/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'todo',
+    'accounts', #login_update
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -120,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = 'home' #login_update
+LOGOUT_REDIRECT_URL = 'home' #login_update
