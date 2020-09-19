38c02580b58fedf70233abcc524222be62d7b1ee
Palak <palakgupta2712@gmail.com>
2020-09-19 19:12:19 +0530

    updates for heroku

--- Django_task_app/settings.py
+++ Django_task_app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'm7r1e-!l&)7kg5!n=50z619h-!47c8)og$mdsl!f0m0(2dq0v('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Django_task_app.wsgi --log-file -
